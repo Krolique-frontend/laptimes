f6ee30a1949c32d005348cd3533e172efdf8c081
krolique <truekrolique@gmail.com>
2021-04-02 20:18:24 +0300

    dbConfig fix

--- dbConfig.js
+++ dbConfig.js
@@ -1,20 +1,35 @@
 class DbConfig {
     constructor(){
+        // for db at https://vh366.timeweb.com/
         this.connConfig = {
-            host: 'localhost',
-            user: "root",
-            database: "laptimes",
-            password: "rhjkbret"
+            host: '188.225.40.161',
+            port: '3306',
+            database: "cq61504_laptimes",
+            user: "cq61504_laptimes",
+            password: "Baseracedays"
         };
+
+        // for local db
+        // this.connConfig = {
+        //     host: 'localhost',
+        //     database: "laptimes",
+        //     user: "root",
+        //     password: "rhjkbret"
+        // };
     }
 
     selectString(selectRow, selectValue) {
         this._selectRow = selectRow;
         this._selectValue = selectValue;
 
-        if (this._selectRow !== undefined && this._selectValue !== undefined) {
+        if (this._selectValue.match(/6km/gi)) {
+            return `SELECT * FROM alltimes WHERE ${this._selectRow} LIKE "%6km%"`;
+        }
+
+        else if (this._selectRow !== undefined && this._selectValue !== undefined) {
             return `SELECT * FROM alltimes WHERE ${this._selectRow}="${this._selectValue}"`;
         }
+
         else return 'SELECT * FROM alltimes';
     }
 
--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const mysql2 = require('mysql2/promise');
-const DbConfig = require('./dbConfig');
-const csv = require('csv-parser');
 const fs = require('fs');
 const cors = require('cors');
 const url = require('url');
+const mysql2 = require('mysql2/promise');
+const express = require('express');
+const app = express();
+
+const DbConfig = require('./dbConfig');
+const Converter = require('./utils/csvToJSON');
 
 app.use(cors());
 app.use(express.static('public/www'));
 
-app.listen(3001, () => {
-    console.log('server running at port 3001...');
-});
-
 async function fetchTable(f, v) {
     let field = f ? f : 'track';
-    let value = v ? v : '6km classic';
+    let value = v ? v : '6km Classic';
     const data = new DbConfig().selectString(field, value);
     const connConf = new DbConfig().connConfig;
     console.log('fetchTable() data:', data);
 
     const connection = await mysql2.createConnection(connConf);
     const [rows] = await connection.execute(data);
-    connection.end();
+    await connection.end();
 
     const outputData = [];
     let tempObj = {};
@@ -62,103 +59,54 @@ async function addNewData(newData) {
     await connection.end();
 }
 
-const converter = (csvFilePath) => {
-    const arr = [];
-
-    fs.createReadStream(csvFilePath)
-        .pipe(csv({separator: ';'}))
-        .on('data', result => arr.push(result))
-        .on('end', () => {
-            // console.log(arr);
-
-            fs.writeFile(`dbJSON.json`, JSON.stringify(arr), (err) => {
-                if (err) console.log('ERROR -', err);
-                else console.log('Document succsessfully converted!');
-                // console.log(arr);
-            });
-        });
-};
-
-app.get('/', (req, res) => {
-    console.log('lalala');
-});
-
-app.get('/convert-csv', (req, res) => {
-    converter('../laptimes-ua/brd_2018_09_22.csv');
-});
-
-// app.get('/initialtable', (req, res) => {
-//     // sending initial react-app page when opening site/app
-//     res.set({'content-type': 'application/json; charset=utf-8'});
-//     fetchTable().then(data => res.json(data));
-// });
-
-// app.get('/event', (req, res) => {
-//     let query = url.parse(req.url, true).query;
-//     let field, fieldValue;
-//     for (let key in query) {
-//         field = key;
-//         fieldValue = query[key];
-//     }
-//     console.log(`get request with params ${field} ${fieldValue}`);
-//
-//     res.set({'content-type': 'application/json; charset=utf-8'});
-//     fetchTable(field, fieldValue).then(data => res.json(data));
-// });
-//
-// app.get('/car', (req, res) => {
-//     let query = url.parse(req.url, true).query;
-//     let field, fieldValue;
-//     for (let key in query) {
-//         field = key;
-//         fieldValue = query[key];
-//     }
-//
-//
-//     console.log(`get request with params ${field} ${fieldValue}`);
-//
-//     res.set({'content-type': 'application/json; charset=utf-8'});
-//     fetchTable(field, fieldValue).then(data => res.json(data));
-// });
-
 const urlWare = async (req, res, next) => {
-    const method = await req.method;
-    const pathname = await url.parse(req.url, true).pathname;
-    const query = await url.parse(req.url, true).query;
+    const method = req.method;
+    const pathname = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
 
     let field, fieldValue;
 
-    for (let key in query) {
-        field = key;
-        fieldValue = query[key];
+    if (!!Object.keys(query).length) {
+        for (let key in query) {
+            field = key;
+            fieldValue = query[key];
+        }
     }
 
     console.log(`get request with params ${field} ${fieldValue}`);
 
-    if (method === 'GET') {
-        switch (pathname) {
-            case '/initialtable':
-                await res.set({'content-type': 'application/json; charset=utf-8'});
-                await fetchTable().then(data => res.json(data));
-        }
+    if (method === 'GET' && pathname === '/initialtable' || pathname === '/event' || pathname === '/car') {
+        await res.set({'content-type': 'application/json; charset=utf-8'});
+        await fetchTable(field, fieldValue).then(data => res.json(data));
     }
 
-    // await res.set({'content-type': 'application/json; charset=utf-8'});
-    // await fetchTable(field, fieldValue).then(data => res.json(data));
+    if (method === 'POST') {
+        switch (pathname) {
+            case '/convert-csv':
+                const converter = new Converter('../laptimes-ua/brd_2018_09_22.csv');
+                converter.convert();
+                res.send('converted');
+                break;
+
+            case '/writetabletodb':
+                const data = JSON.parse(fs.readFileSync("dbJSON.json"));
+                addNewData(data);
+                res.send('Данные внесены в базу... но это не точно.');
+                break;
+        }
+    }
 
     await next();
 };
 
 app.use(urlWare);
 
-app.post('/writetabletodb', (req, res) => {
-    const data = JSON.parse(fs.readFileSync("dbJSON.json"));
-    addNewData(data);
-    res.send('Данные внесены в базу... но это не точно.');
-});
-
-const unknownPath = (req,res) => {
+const unknownPath = (req, res) => {
     res.status(404).send({error: 'no such page/resource'});
 };
 
 app.use(unknownPath);
+
+app.listen(3001, () => {
+    console.log('server running at port 3001...');
+});
